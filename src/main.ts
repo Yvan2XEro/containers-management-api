15bf6c12eb6cc2c366533568530de876f6536cc9
Yvan2xEro <yvankanaj@gmail.com>
2024-06-17 11:05:53 +0100

    tester

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,12 @@ if (!process.env.IS_TS_NODE) {
 }
 
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-
-  const port = configService.get<number>('DATABASE_PORT');
-
   app.enableCors();
 
 
